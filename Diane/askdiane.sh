95374d04502d6e4958dcfc40231f5cc6d7b2bdfa
Steven K Word <stevenword@gmail.com>
2014-01-29 19:45:14 -0500

    Update paths

--- Diane/askdiane.sh
+++ Diane/askdiane.sh
@@ -2,14 +2,14 @@
 
 #echo "Recording... Press Ctrl+C to Stop."
 
-#/home/pi/PiAUISuite/Personas/Diane/speech2text.sh
+#./speech2text.sh
 
 #QUESTION=$(cat stt.txt)
 
 QUESTION=$1
 echo "Me: ", $QUESTION
 
-ANSWER=$(python /home/pi/PiAUISuite/Personas/Diane/querywolfram.py $QUESTION)
+ANSWER=$(python ./querywolfram.py $QUESTION)
 echo "Robot: ", $ANSWER
 
-/home/pi/PiAUISuite/Personas/Diane/text2speech.sh $ANSWER
+./text2speech.sh $ANSWER
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#
 ./lib/PiAUISuite/Install/InstallAUISuite.sh
